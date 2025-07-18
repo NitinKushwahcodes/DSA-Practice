901944d54beb63f9f69114a1eb624b31e5ca63df
NitinKushwahcodes <xyzcoding06@gmail.com>
2025-07-18 23:24:45 +0530

    Time: 8 ms (44.26%), Space: 38.7 MB (50.71%) - LeetHub

---
+++ 1708-design-parking-system/1708-design-parking-system.cpp
@@ -0,0 +1,17 @@
+class ParkingSystem {
+private:
+    int s[4];
+public:
+    ParkingSystem(int big,int medium,int small){
+        s[1]=big;
+        s[2]=medium;
+        s[3]=small;
+    }
+    bool addCar(int t){
+        if(s[t]>0){
+            s[t]--;
+            return true;
+        }
+        return false;
+    }
+};
