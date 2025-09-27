40752c580360afa98665c0108c57e31b2f911612
NitinKushwahcodes <xyzcoding06@gmail.com>
2025-09-27 14:18:36 +0530

    Time: 23 ms (53.4%), Space: 17.4 MB (55.38%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,29 +1,36 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums,int target){
-        vector<vector<int>> ans;
-        int n=nums.size();
-        if(n<4) return ans;
-        sort(nums.begin(),nums.end());
-        for(int i=0;i<n-3;i++){
-            if(i>0&&nums[i]==nums[i-1]) continue;
-            for(int j=i+1;j<n-2;j++){
-                if(j>i+1&&nums[j]==nums[j-1]) continue;
-                int l=j+1,r=n-1;
-                long long t=(long long)nums[i]+nums[j];
-                while(l<r){
-                    long long s=t+nums[l]+nums[r];
-                    if(s<target){
-                        l++;
+        sort(nums.begin(), nums.end());
+        vector<vector<int>>ans;
+        int n = nums.size();
+        for(int i=0; i<n-3; i++){
+            if(i>0 && nums[i] == nums[i-1]){
+                continue;
+            }
+            for(int j=i+1; j<n-2; j++){
+                if(j>i+1 && nums[j] == nums[j-1]){
+                    continue;
+                }
+                int p=j+1, k=n-1;
+                while(p<k){
+                    long long sum = (long long)nums[i]+nums[j]+nums[p]+nums[k];
+                    if(sum == target){
+                        ans.push_back({nums[i], nums[j], nums[p], nums[k]});
+                        p++;
+                        k--;
+                        while(p<k && nums[p] == nums[p-1]){
+                           p++;
+                        }
+                        while(p<k && nums[k] == nums[k+1]){
+                            k--;
+                        }
                     }
-                    else if(s>target){
-                        r--;
+                    else if(sum < target){
+                        p++;
                     }
                     else{
-                        ans.push_back({nums[i],nums[j],nums[l],nums[r]});
-                        int x=nums[l],y=nums[r];
-                        while(l<r&&nums[l]==x) l++;
-                        while(l<r&&nums[r]==y) r--;
+                        k--;
                     }
                 }
             }
