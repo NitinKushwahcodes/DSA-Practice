2c9fe2cf64f7c8c8261f16389599709545cdec93
NitinKushwahcodes <xyzcoding06@gmail.com>
2025-10-18 19:02:52 +0530

    Time: 22 ms (59.11%), Space: 70.6 MB (14.17%) - LeetHub

--- 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -9,7 +9,7 @@ public:
                     cl_bq++;
                     cl_fl = 0;
                     if(cl_bq >= m){
-                        return true;
+                       return cl_bq;
                     }
                 }
             }
@@ -17,24 +17,22 @@ public:
                 cl_fl = 0;
             }
         }
-        return cl_bq>=m;
+        return cl_bq;
     }
     int minDays(vector<int>& bloomDay, int m, int k) {
-        int ans = -1;
         int low = *min_element(bloomDay.begin(), bloomDay.end());
         int high = *max_element(bloomDay.begin(), bloomDay.end());
         if(1LL*m*k > bloomDay.size()) return -1;
         while(low <= high){
             int mid = low +(high-low)/2;
             int val = check(bloomDay, mid, m, k);
-            if(val){
-                ans = mid;
-                high = mid - 1;
+            if(val >= m){
+                high = mid-1;
             }
             else{
                 low = mid+1;
             }
         }
-        return ans;
+        return low;
     }
 };
